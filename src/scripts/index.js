0ff31013aa81e91a26c9db4034d8b1ac4ff33faf
Wynn Slater <wynn.slater@ul.com>
2016-04-20 11:30:30 -0700

    Remove outliner stuff for now

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,90 +1,34 @@
 (function () {
 
-var app = angular.module("wynn", []);
+	var app = angular.module("wynn", []);
 
-app.controller("outlineController", ["$scope", "$window",
-	function ($scope, $window) {
-		var model = {
-			menu:false
-		};
+	app.controller("cvController", ["$scope", "$window",
+		function ($scope, $window) {
+			var model = {
+				menu:false
+			};
 
-		var localStorage = $window.localStorage;
-		if (localStorage) {
-			model.address = getAddress();
-			model.phone = localStorage.phone;
+			var localStorage = $window.localStorage;
+			if (localStorage) {
+				model.address = getAddress();
+				model.phone = localStorage.phone;
+			}
+
+			$scope.model = model;
+
+			function getAddress () {
+				var address = localStorage.address;
+				if (!address) {
+					return false;
+				}
+				try {
+					address = JSON.parse(address);
+				} catch (e) {
+					/* if it's not valid, it's not valid */
+				}
+				return address;
+			}
 		}
-
-		$scope.model = model;
-
-		function getAddress () {
-			var address = localStorage.address;
-			if (!address) {
-				return false;
-			}
-			try {
-				address = JSON.parse(address);
-			} catch (e) {
-				/* if it's not valid, it's not valid */
-			}
-			return address;
-		}
-	}
-]);
-
-app.directive("outline", [
-	function () {
-		return {
-			apply: "A",
-			require: {
-				parentController: "?^^outline",
-				ownController: "outline"
-			},
-			scope: {
-				outline: "=?outline"
-			},
-			controller: ["$scope", "$attrs", function ($scope, $attrs) {
-
-				var module = {
-					id: undefined,
-					label: "",
-					children: [],
-					init: init,
-					registerChild: registerChild
-				};
-
-				function init (cfg) {
-					module.id = cfg.id;
-					module.label = cfg.label;
-				}
-
-				function registerChild (ctrl) {
-					module.children.push(ctrl);
-					if ($attrs.outline) {
-						$scope.outline = module.children;
-					}
-				}
-
-				return module;
-			}],
-			link: function (scope, el, attrs, ctrls) {
-				ctrls.ownController.init({
-					id: el.attr("id"),
-					// label: el.text()
-					label: el.attr("id")
-				});
-				if(ctrls.parentController) {
-					ctrls.parentController.registerChild(ctrls.ownController);
-				}
-			}
-		};
-	}
-]);
-
-
-
-
-
-
-
+	]);
 
 })();
